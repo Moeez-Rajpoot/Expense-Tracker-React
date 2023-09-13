6fc4578c9dc98a80c448006b0c6060b4098d24e0
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-09-13 22:53:25 +0500

    Updated

--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -1,20 +1,19 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import './ExpenseItem.css'
+import React from "react";
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
   return (
     <>
-      <div className='Expense-item-main'>
+      <div className="Expense-item-main">
         <h3>Sep 13 2023</h3>
-      <div className='Expense-item-details'>
-        <h2>Car Insurance</h2>
-        <div className='Expense-item-price'>$300</div>
+        <div className="Expense-item-details">
+          <h2>Car Insurance</h2>
+          <div className="Expense-item-price">$300</div>
+        </div>
       </div>
-      </div>
-       
     </>
-  )
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
