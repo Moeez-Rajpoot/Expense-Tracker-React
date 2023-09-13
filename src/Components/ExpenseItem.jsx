f385f391bcde3fb1e3fab74e616c363c79e3fb28
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-09-13 22:42:56 +0500

    Updated

--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -1,10 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import './ExpenseItem.css'
 
 function ExpenseItem() {
   return (
     <>
-      <h2>This is Expense Tracker item </h2>
+      <div className='Expense-item-main'>
+        <h3>Sep 13 2023</h3>
+      <div className='Expense-item-details'>
+        <h2>Car Insurance</h2>
+        <div className='Expense-item-price'>$300</div>
+      </div>
+      </div>
+       
     </>
   )
 }
