7fed4c2e29d1ec7a9d4354970c76dff5df899c50
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-09-13 21:54:19 +0500

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
+import ExpenseItem from './Components/ExpenseItem'
 function App() {
  
 
   return (
     <>
-      <h1>hello</h1>
-       
+    <ExpenseItem/>
     </>
   )
 }
---
+++ src/Components/ExpenseItem.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function ExpenseItem() {
+  return (
+    <>
+      <h2>This is Expense Tracker item </h2>
+    </>
+  )
+}
+
+export default ExpenseItem
