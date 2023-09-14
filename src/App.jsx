55f867157898f55b4e376fc66be1ebdb6d7c919f
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-09-14 18:47:26 +0500

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
 import ExpenseItem from "./Components/ExpenseItem";
 function App() {
+
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
     <>
-      <ExpenseItem />
+    <h1>Expense List</h1>
+
+     {expenses.map((expense) =>(
+      <ExpenseItem key={expense.id} expense={expense}/>
+     ))}
     </>
   );
 }
--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -1,15 +1,26 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import "./ExpenseItem.css";
+import PropTypes from "prop-types";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const month = props.expense.date.toLocaleString("en-US", { month: "long" });
+  const day = props.expense.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.expense.date.getFullYear();
+  const price = props.expense.amount;
+  const title = props.expense.title;
+
   return (
     <>
       <div className="Expense-item-main">
-        <h3>Sep 13 2023</h3>
+        <div className="Expense-item-date">
+          <h3>{month}</h3>
+          <h3>{day}</h3>
+          <h3>{year}</h3>
+        </div>
         <div className="Expense-item-details">
-          <h2>Car Insurance</h2>
-          <div className="Expense-item-price">$300</div>
+          <h2>{title}</h2>
+          <div className="Expense-item-price">${price}</div>
         </div>
       </div>
     </>
@@ -17,3 +28,11 @@ function ExpenseItem() {
 }
 
 export default ExpenseItem;
+
+ExpenseItem.propTypes = {
+  expense: PropTypes.shape({
+    date: PropTypes.instanceOf(Date).isRequired,
+    amount: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+};
