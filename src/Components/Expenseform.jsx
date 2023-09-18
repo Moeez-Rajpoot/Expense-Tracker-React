718ffa54b88ac6a2affeaf81aa9ac9c107be807e
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-09-18 22:57:44 +0500

    Updated

--- src/Components/Expenseform.jsx
+++ src/Components/Expenseform.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
 import './Expenseform.css';
 function Expenseform() {
   return (
     <>
 
-    <div className="form-main">
-        <form>
+        <form className="form-main">
             <div className="title-field">
                 <label>Title</label>
-                <input type="text"/>
+                <input type="text" placeholder='Enter Title Here'/>
             </div>
+            <div className="price-field">
+                <label>Price</label>
+                <input type="number" min="0.01" step="0.01" placeholder='Enter Price Here' />
+            </div>
+            <div className="date-field">
+                <label>Date</label>
+                <input type="date" min="2019-01-01" max="2023-31-12"/>
+            </div>
+            
+                <button className='button-field' type="submit">Add Expense</button>
+    
         </form>
 
 
-    </div>
+
 
       
     </>
