20e67693ad615b5596aef95c485aac4be49f819c
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-09-20 19:43:13 +0500

    Updated

--- src/Components/Expenseform.jsx
+++ src/Components/Expenseform.jsx
@@ -1,45 +1,59 @@
-import { useState } from 'react';
-import './Expenseform.css';
+import { useState } from "react";
+import "./Expenseform.css";
 function Expenseform() {
-    const [title , settitle ] = useState('');
-    const [price , setprice ] =useState('');
-    const [date , setdate ] = useState('');
+  const [title, settitle] = useState("");
+  const [price, setprice] = useState("");
+  const [date, setdate] = useState("");
 
-    function TitleChangeHandler(event) {
-        settitle(event.target.value);
-        
-    }
+  function TitleChangeHandler(event) {
+    settitle(event.target.value);
+  }
 
-    function PriceChangeHandler(event) {
-        setprice(event.target.value);
-    }   
+  function PriceChangeHandler(event) {
+    setprice(event.target.value);
+  }
 
-    function DateChangeHandler(event) {         
-        setdate(event.target.value);
-    }
+  function DateChangeHandler(event) {
+    setdate(event.target.value);
+  }
 
-    return (
-        <>
+  return (
+    <>
+      <form className="form-main">
+        <div className="title-field">
+          <label>Title</label>
+          <input
+            type="text"
+            placeholder="Enter Title Here"
+            onChange={TitleChangeHandler}
+          />
+        </div>
+        <div className="price-field">
+          <label>Price</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            placeholder="Enter Price Here"
+            onChange={PriceChangeHandler}
+          />
+        </div>
+        <div className="date-field">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2023-31-12"
+            onChange={DateChangeHandler}
+          />
+        </div>
 
-        <form className="form-main">
-            <div className="title-field">
-                <label>Title</label>
-                <input type="text" placeholder='Enter Title Here' onChange={TitleChangeHandler}/>
-            </div>
-            <div className="price-field">
-                <label>Price</label>
-                <input type="number" min="0.01" step="0.01" placeholder='Enter Price Here' onChange={PriceChangeHandler} />
-            </div>
-            <div className="date-field">
-                <label>Date</label>
-                <input type="date" min="2019-01-01" max="2023-31-12" onChange={DateChangeHandler}/>
-            </div>
-            
-                <button className='button-field' type="submit">Add Expense</button>
-        </form>
-
+        <button className="button-field" type="submit">
+          Add Expense
+        </button>
+      </form>
     </>
-  )
+  );
 }
 
-export default Expenseform
+export default Expenseform;
