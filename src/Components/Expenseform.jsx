78e955482a83df23f7f3e935f8b970a866f12fdc
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-09-20 19:25:47 +0500

    Updated

--- src/Components/Expenseform.jsx
+++ src/Components/Expenseform.jsx
@@ -1,20 +1,38 @@
+import { useState } from 'react';
 import './Expenseform.css';
 function Expenseform() {
-  return (
-    <>
+    const [title , settitle ] = useState('');
+    const [price , setprice ] =useState('');
+    const [date , setdate ] = useState('');
+
+    function TitleChangeHandler(event) {
+        settitle(event.target.value);
+        
+    }
+
+    function PriceChangeHandler(event) {
+        setprice(event.target.value);
+    }   
+
+    function DateChangeHandler(event) {         
+        setdate(event.target.value);
+    }
+
+    return (
+        <>
 
         <form className="form-main">
             <div className="title-field">
                 <label>Title</label>
-                <input type="text" placeholder='Enter Title Here'/>
+                <input type="text" placeholder='Enter Title Here' onChange={TitleChangeHandler}/>
             </div>
             <div className="price-field">
                 <label>Price</label>
-                <input type="number" min="0.01" step="0.01" placeholder='Enter Price Here' />
+                <input type="number" min="0.01" step="0.01" placeholder='Enter Price Here' onChange={PriceChangeHandler} />
             </div>
             <div className="date-field">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2023-31-12"/>
+                <input type="date" min="2019-01-01" max="2023-31-12" onChange={DateChangeHandler}/>
             </div>
             
                 <button className='button-field' type="submit">Add Expense</button>
